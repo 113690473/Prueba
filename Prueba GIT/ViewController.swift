abaa1c0080f79e64807224d27cc25fdf809c55c2
Diana Carvajal <co.diana@outlook.com>
2019-07-12 17:36:14 -0600

    Initial Commit

---
+++ Prueba GIT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prueba GIT
+//
+//  Created by Andres Marten on 7/12/19.
+//  Copyright © 2019 GigiCo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
